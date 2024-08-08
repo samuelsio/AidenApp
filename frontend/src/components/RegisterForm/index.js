1e17b2c174e9dfc58c434df9bd9c54e93a258212
Aiden <aidenheath4042@outlook.com>
2024-08-08 14:13:41 +0100

    Using correct scss file now

--- frontend/src/components/RegisterForm/index.js
+++ frontend/src/components/RegisterForm/index.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import regPoster from "../../images/RegisterPoster.jpg";
 import Button from '../Button';
+import "./RegisterForm.scss"
+
+
 
 export default function RegisterForm() {
   const [password, setPassword] = useState("");
@@ -12,8 +15,6 @@ export default function RegisterForm() {
   const [lName, setLName] = useState("");
   const [dob, setDOB] = useState("");
   const [gender, setGender] = useState("");
- 
-  
 
   useEffect(() => {
     if (password === confirmPassword) {
@@ -22,7 +23,7 @@ export default function RegisterForm() {
       console.log("No match")
     }
   }, [password, confirmPassword])
-
+  
   useEffect(() => {
     console.log({
       password,
@@ -43,7 +44,7 @@ export default function RegisterForm() {
               <h1 className='title' >Welcome {username}!</h1>
               <p className='paragraph'>Sign up now and start searching for new friends, join public events, create new clans, meet people to play the games you enjoy, and find related games on sale</p>
           </div>
-          <form className='login-form'>
+          <form className='register-form'>
               <input className='form-input' type="text" placeholder='Username' onChange={(event) => {setUsername(event.target.value)}} />
               <input className='form-input' type="text" id='email' placeholder='Email' onChange={(event) => {setEmail(event.target.value)}} />
               <input className='form-input' type='text' id='fName' placeholder='First Name' onChange={(event) => {setFName(event.target.value)}} />
@@ -51,12 +52,12 @@ export default function RegisterForm() {
               <input className='form-input' type="password" name='confirm-password' id='confirm-password'  placeholder='Confirm Password' onChange={(event) => {setPassword(event.target.value)}} />
               <input className='form-input' type="password" name='password' id='password'pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" placeholder='Password' title='Password must contain: at least 8 characters, an Uppercase, a Lowercase letterm and a number' onChange={(event) => {setConfirmPassword(event.target.value)}} />
               <input className='form-input' type="date" name='dob' id='dob' onChange={(event) => {setDOB(event.target.value)}} />
-                <input className='form-input' type='radio' name='gender' id='male' value='male' />
-                  <label for="male">Male</label>
-                <input className='form-input' type='radio' name='gender' id='female' value='female' />
-                  <label for="female">Female</label>  
-                <input className='form-input' type='radio' name='gender' id='other' value='other' />
-                  <label for="other">Other</label>
+                <input className='form-input' type='radio' name='gender' id='male' value='Male' onChange={(event) => {setGender(event.target.value)}}/>
+                  <label htmlFor="male">Male</label>
+                <input className='form-input' type='radio' name='gender' id='female' value='Female' onChange={(event) => {setGender(event.target.value)}}/>
+                  <label htmlFor="female">Female</label>  
+                <input className='form-input' type='radio' name='gender' id='other' value='Other' onChange={(event) => {setGender(event.target.value)}}/>
+                  <label htmlFor="other">Other</label>
               <Button variant="primary" large={false} disabled={(password === confirmPassword) ? false : true} label='Create Account' type='button' fontSize='large' fullWidth={false}/>  
 
               </form>
