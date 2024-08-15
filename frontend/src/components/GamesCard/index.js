9a27741e816d6234a20410d51e7182030913e8b4
Aiden <aidenheath4042@outlook.com>
2024-08-15 23:02:32 +0100

    Added twitter embed and basic stylisation

--- frontend/src/components/GamesCard/index.js
+++ frontend/src/components/GamesCard/index.js
@@ -1,42 +1,58 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import "./GamesCard.scss"
 import gamesData from "../../pages/Games/dataGames.json";
 
 
 export default function GamesCard() {
+    useEffect(() => {
+        const loadTwitterWidget = (index) => {
+            if(index >= gamesData.games.length) return;
+            const script = document.createElement('script');
+            script.src = "https://platform.twitter.com/widgets.js";
+            script.async = true;
+            script.charSet = "utf-8";
+            document.body.appendChild(script);
+            setTimeout(() => loadTwitterWidget(index +1), 1000)
+        };
+        loadTwitterWidget(0);
+        }, []);
 
-
-    return(
+        return(
         gamesData.games.map((game) => (
             <div className='GamesCard__container' key={game.id}>
-                <div className='GamesCard__name'>
+                <div className='GamesCard'>
+                    <div className='GamesCard__name'>
                     <h1 className='GamesCard__name__header'>{game.name}</h1>
-                </div>
-                <div className='GamesCard__image'>
-                    <img src={game.image} alt={game.name} />
-                </div>
-                <div className='GamesCard__description'>
-                    <p className='GamesCard__description__text'>{game.description}</p>
-                </div>
-                <div className='GamesCard__twitter_handle'>
-                    <a className="twitter-timeline" href="https://twitter.com/helldivers2?ref_src=twsrc%5Etfw">Tweets by helldivers2</a> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-                </div>
-                <div className='GamesCard__thumbnail'>
-                    <img src={game.thumbnail} alt={game.name} />
-                </div>
-                {Object.entries(game.store).map(([storeName, storeUrl]) => (
-                    <div className='GamesCard__store' key={storeName}>
-                        <a href={storeUrl} target="_blank" rel="noopener noreferrer">{storeName}</a>
+                        {Object.entries(game.store).map(([storeName, storeUrl]) => (
+                            <div className='GamesCard__store' key={storeName}>
+                                <a className='GamesCard__store__link' href={storeUrl} target="_blank" rel="noopener noreferrer">{storeName}</a>
+                            </div>
+                        ))}
                     </div>
-                ))}
-
+                    <div className='GamesCard__content'>
+                        <div className='GamesCard__image'>
+                            <img src={game.img_url} alt={game.name} />
+                        </div>
+                        <div className='GamesCard__description'>
+                            <p className='GamesCard__description__text'>{game.description}</p>
+                        </div>
+                        <div className='GamesCard__twitter_handle'>
+                            <a className="twitter-timeline" data-width="400" data-height="256" data-theme="dark" href={`https://twitter.com/${game.twitter_handle}?ref_src=twsrc%5Etfw`}>Tweets by helldivers2</a>
+                        </div>
+                        <div className='GamesCard__thumbnail'>
+                            <img src={game.thumbnail} alt={game.name} />
+                        </div>
+                        <div className='GamesCard__store_wrapper'>
+                            
+                        </div>
+                    </div>
 
 
 
 
                 
 
-
+                </div>
             </div>
         ))
     )
