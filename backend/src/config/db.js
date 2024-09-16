ff9043c13578adebd4f061491469153b116624bb
Ray Samuels <ray@samuels.io>
2024-09-16 17:18:55 +0100

    AR-56: Updated login details for endpoints web-server to access database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
+    host: "localhost",
+    port: 5432,
+    database: "testdb",
+    user: "app_user",
+    password: "app_password",
 });
 
 module.exports = pool;
