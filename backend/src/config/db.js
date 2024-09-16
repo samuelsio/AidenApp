0bc2274a4a8df2156b48ac868004470176945aa7
Ray Samuels <ray@samuels.io>
2024-09-16 16:57:13 +0100

    AR-56: Initialised postgres local db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,12 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
 });
 
 module.exports = pool;
