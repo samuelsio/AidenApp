69c7254f0de65c4ba84baeb4dcb1eed91484b987
Ray Samuels <ray@samuels.io>
2024-09-16 19:17:58 +0100

    AR56 ALT: Postgres without docker

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    user: "app_user",
+    host: "localhost",
+    database: "testdb",
+    password: "password",
+    port: 5432,
 });
 
 module.exports = pool;
