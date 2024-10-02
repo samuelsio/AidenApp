34f59542f2da737bcf76da4bce4f09b59e16dfda
Aiden <aidenheath4042@outlook.com>
2024-10-02 10:48:52 +0100

    Addition of membership table
    Used to track who belongs to what clan

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -173,6 +173,42 @@ ALTER SEQUENCE public.events_event_id_seq OWNED BY public.events.event_id;
 
 
 --
+-- Name: membership; Type: TABLE; Schema: public; Owner: app_user
+--
+
+CREATE TABLE public.membership (
+    membership_id integer NOT NULL,
+    user_id integer NOT NULL,
+    clan_id integer NOT NULL,
+    joined_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.membership OWNER TO app_user;
+
+--
+-- Name: membership_membership_id_seq; Type: SEQUENCE; Schema: public; Owner: app_user
+--
+
+CREATE SEQUENCE public.membership_membership_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.membership_membership_id_seq OWNER TO app_user;
+
+--
+-- Name: membership_membership_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: app_user
+--
+
+ALTER SEQUENCE public.membership_membership_id_seq OWNED BY public.membership.membership_id;
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: app_user
 --
 
@@ -249,6 +285,13 @@ ALTER TABLE ONLY public.events ALTER COLUMN event_id SET DEFAULT nextval('public
 
 
 --
+-- Name: membership membership_id; Type: DEFAULT; Schema: public; Owner: app_user
+--
+
+ALTER TABLE ONLY public.membership ALTER COLUMN membership_id SET DEFAULT nextval('public.membership_membership_id_seq'::regclass);
+
+
+--
 -- Name: users user_id; Type: DEFAULT; Schema: public; Owner: app_user
 --
 
@@ -292,8 +335,8 @@ COPY public.comments (comment_id, content, creation_date, author_id, event_id) F
 3	Can’t wait for the duel!	2024-09-17 11:52:52.382228	8	3
 4	This is a comment	2024-09-19 15:13:34.117251	6	12
 5	This is a comment	2024-09-19 15:16:17.821768	6	12
-6	This is a comment	2024-09-19 15:16:18.864478	6	12
 7	This is a comment	2024-09-20 10:34:29.540706	6	12
+6	This is a patched field	2024-09-19 15:16:18.864478	6	12
 \.
 
 
@@ -315,6 +358,26 @@ COPY public.events (event_id, title, description, event_date, creator_id, clan_i
 11	epic gaming time	TestDescription	2024-09-19 14:13:39.972502	2	1
 12	epic gaming time	TestDescription	2024-09-19 14:13:42.272898	2	1
 13	epic gaming time	TestDescription	2024-09-20 09:58:33.011178	2	1
+\.
+
+
+--
+-- Data for Name: membership; Type: TABLE DATA; Schema: public; Owner: app_user
+--
+
+COPY public.membership (membership_id, user_id, clan_id, joined_at) FROM stdin;
+1	1	1	2024-09-27 14:44:24.107425
+2	2	1	2024-09-27 14:44:24.107425
+3	3	1	2024-09-27 14:44:24.107425
+4	4	2	2024-09-27 14:44:24.107425
+5	5	2	2024-09-27 14:44:24.107425
+6	6	2	2024-09-27 14:44:24.107425
+7	7	3	2024-09-27 14:44:24.107425
+8	8	3	2024-09-27 14:44:24.107425
+9	9	3	2024-09-27 14:44:24.107425
+10	10	1	2024-09-27 14:44:24.107425
+11	11	1	2024-09-27 14:44:24.107425
+12	12	2	2024-09-27 14:44:24.107425
 \.
 
 
@@ -375,6 +438,13 @@ SELECT pg_catalog.setval('public.events_event_id_seq', 13, true);
 
 
 --
+-- Name: membership_membership_id_seq; Type: SEQUENCE SET; Schema: public; Owner: app_user
+--
+
+SELECT pg_catalog.setval('public.membership_membership_id_seq', 1, false);
+
+
+--
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: app_user
 --
 
@@ -411,6 +481,22 @@ ALTER TABLE ONLY public.comments
 
 ALTER TABLE ONLY public.events
     ADD CONSTRAINT events_pkey PRIMARY KEY (event_id);
+
+
+--
+-- Name: membership membership_pkey; Type: CONSTRAINT; Schema: public; Owner: app_user
+--
+
+ALTER TABLE ONLY public.membership
+    ADD CONSTRAINT membership_pkey PRIMARY KEY (membership_id);
+
+
+--
+-- Name: membership membership_user_id_clan_id_key; Type: CONSTRAINT; Schema: public; Owner: app_user
+--
+
+ALTER TABLE ONLY public.membership
+    ADD CONSTRAINT membership_user_id_clan_id_key UNIQUE (user_id, clan_id);
 
 
 --
@@ -491,6 +577,22 @@ ALTER TABLE ONLY public.comments
 
 ALTER TABLE ONLY public.clans
     ADD CONSTRAINT fk_leader FOREIGN KEY (leader_id) REFERENCES public.users(user_id) ON DELETE SET NULL;
+
+
+--
+-- Name: membership membership_clan_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: app_user
+--
+
+ALTER TABLE ONLY public.membership
+    ADD CONSTRAINT membership_clan_id_fkey FOREIGN KEY (clan_id) REFERENCES public.clans(clan_id) ON DELETE CASCADE;
+
+
+--
+-- Name: membership membership_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: app_user
+--
+
+ALTER TABLE ONLY public.membership
+    ADD CONSTRAINT membership_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
 
 
 --
--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -10,6 +10,57 @@ exports.getUsers = async (req, res) => {
     }
 };
 
+exports.getAllMemberships = async (req, res) => {
+    try {
+        const memberships = await userModel.getAllMemberships();
+        res.status(200).json(memberships)
+    } catch (err){
+        console.error(err);
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+exports.getMembershipDetail = async (req, res) => {
+    try {
+        const clanId = parseInt(req.params.clanId)
+        const userId = parseInt(req.params.userId)
+
+        if (!userId && !clanId){
+            res.status(404).json({error: `No clanId and No userId`})
+        }
+
+        if (!userId){
+            const membershipFromClan = await userModel.getMembershipDetail({clan_id: clanId})
+            res.status(200).json({membershipFromClan})
+        }
+
+        if (!clanId){
+            const membershipFromUser = await userModel.getMembershipDetail({user_id: userId})
+            res.status(200).json({membershipFromUser})
+        }
+    } catch (err){
+        console.error(`Error getting MembershipDetail: ${err}`)
+        throw err;
+    }
+}
+
+exports.getMembershipDetailedView = async (req, res) => {
+    try { 
+        const userId = parseInt(req.params.userId);
+        const clanId = parseInt(req.params.clanId);
+
+        if (!userId || !clanId){
+            return res.status(404).json({ message: `missing fields userId: ${userId}, clanId: ${clanId}`})
+
+        }
+        const memberships = await userModel.getMembershipDetailedView({clan_id: clanId, user_id: userId})
+        res.status(200).json({memberships})
+    } catch (err){
+        console.error(err)
+        res.status(500).json({ error: `Server error: ${err.message}`})
+    }
+};
+
 exports.getUserDetails = async (req, res) => {
     try {
         const { userId } = req.params;
@@ -25,6 +76,19 @@ exports.getUserDetails = async (req, res) => {
     }
 }
 
+exports.createMembership = async (req, res) => {
+    try { 
+        const membership = await userModel.createMembership(req.body);
+        res.status(201).json({
+            message: `Member added with ID: ${membership.membership_id}`,
+            membership: membership
+        })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: `Server error: ${err.message}`})
+    }
+}
+
 exports.createUser = async (req, res) => {
     try {
         const user = await userModel.createUser(req.body);
@@ -35,6 +99,34 @@ exports.createUser = async (req, res) => {
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Server error" });
+    }
+};
+
+exports.updateMembership = async (req, res) => {
+    try {
+        const membershipId = parseInt(req.params.membershipId)
+        if (isNaN(membershipId)){
+            return res.status(400).json({ error: `Invalid membership ID: ${membershipId}` });  // Handle invalid userId
+        }
+
+        const updatedFields = req.body;
+        const updatedMembership = await userModel.updateMembership({
+            membership_id: membershipId, 
+            updatedFields: updatedFields
+        });
+        if (updatedMembership === 0) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        res.status(201).json({message: `User updated successfully`, updatedMembership: updatedMembership});
+
+    } catch (err) {
+        console.error(err);
+        if (err.message === "No valid fields provided for update") {
+            res.status(400).json({ error: err.message });
+        } else {
+            res.status(500).json({ error: "Server error" });
+        }
     }
 };
 
@@ -65,6 +157,24 @@ exports.updateUser = async (req, res) => {
     }
 };
 
+exports.deleteMembership = async (req, res) => {
+    try { 
+        const membershipId = parseInt(req.params.membershipId);
+        if (isNaN(membershipId)) {
+            return res.status(400).json({ error: `Invalid format for membershipId: ${membershipId}`})
+
+        }
+
+        const deleteMembership = await userModel.deleteMembership({membership_id: membershipId})
+        return res.status(202).json({
+            message: "Membership deleted",
+            membership: deleteMembership
+        })
+    } catch (err){
+        res.status(500).json({ error: `Server error: ${err}`})
+    }
+};
+
 exports.deleteUser = async (req, res) => {
     try {
         const user_id = parseInt(req.params.userId);
@@ -79,4 +189,4 @@ exports.deleteUser = async (req, res) => {
     } catch (err){
         res.status(500).json({ error: `Server error: ${err}`})
     }
-}
+};
